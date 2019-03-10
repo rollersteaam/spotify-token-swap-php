d6429e23b9440324555f89ea6965a4d012ad1f7b
Jordan <rollersteaam@googlemail.com>
2019-03-10 14:44:26 +0000

    Adapt to new convention

--- redirect.php
+++ redirect.php
@@ -1,5 +1,5 @@
 <?php
-    header("Location: http://localhost:4002/?".http_build_query([
+    header("Location: http://localhost:4002/auth?".http_build_query([
         "code" => $_GET["code"],
         "state" => $_GET["state"]
     ]), true, 303);
